226e8f5de920ff95a7db98bbf31bb05bc660c786
hijas ca <hijascawork@gmail.com>
2025-11-18 15:19:34 +0530

    recursion code has done here

--- net.py
+++ net.py
@@ -2,6 +2,14 @@ print("hello hey")
 print("conflict avatte")
 print("conflict veendum")
 print("second line changed by hijas ca")
-
 print("hijas aaane")
-master
+list = [1,2,4,[2,35,4,[2,34,3]]]
+def re(list_):
+    new = []
+    for each_item in list_:
+        if isinstance(list,list_):
+            new.extend(re(each_item))
+        else:
+            new.append(each_item)
+
+
