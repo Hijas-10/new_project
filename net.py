b1f110ace2d83b947f67e1dd91e849fbafe1f66c
hijas ca <hijascawork@gmail.com>
2025-11-18 14:50:36 +0530

    puthiya preshnam

--- net.py
+++ net.py
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 print("hello hey")
+=======
+print("hello hey")
+print("conflict avatte")
+print("second line changed by hijas ca")
+print("hijas aaane")
+>>>>>>> f2b6be9 (puthiya preshnam)
